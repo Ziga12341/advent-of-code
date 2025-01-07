1613442b6a672b46b873963893895eb940a4d92e
Žiga Pregelj <ziga.pregelj@razum.si>
2025-01-07 15:00:03 +0100

    feat: 2020 day 1 done with optimization with generator

--- 2020/Day 01/main.py
+++ 2020/Day 01/main.py
@@ -7,7 +7,7 @@ l = "input.txt"
 
 def read_lines(file_name: str) -> list:
     with open(file_name, "r", encoding="utf-8") as file:
-        return [int(line.strip()) for line in file]
+        return {int(line.strip()) for line in file}
 
 
 small_input: list[str] = read_lines(s)
@@ -21,13 +21,17 @@ def find_values_that_sum_is_2020(file_name):
             if first + second == 2020:
                 return first * second
 
-
+# optimized solution
 def find_values_that_sum_is_2020(file_name):
     whole_list_of_expenses = read_lines(file_name)
     for first in whole_list_of_expenses:
         if 2020 - first in whole_list_of_expenses:
             return first * (2020 - first)
 
+# optimized solution with generator
+def find_values_that_sum_is_2020(file_name):
+    whole_list_of_expenses = read_lines(file_name)
+    return next(first * (2020 - first) for first in whole_list_of_expenses if 2020 - first in whole_list_of_expenses)
 
 def find_three_values_that_sum_2020(file_name):
     whole_list_of_expenses = read_lines(file_name)
@@ -37,14 +41,17 @@ def find_three_values_that_sum_2020(file_name):
                 if first + second + third == 2020:
                     return first * second * third
 
-
+# optimized solution
 def find_three_values_that_sum_2020(file_name):
     whole_list_of_expenses = read_lines(file_name)
     for first in whole_list_of_expenses:
         for second in whole_list_of_expenses:
             if 2020 - first - second in whole_list_of_expenses:
                 return first * second * (2020 - first - second)
-
+# with generatto
+def find_three_values_that_sum_2020(file_name):
+    whole_list_of_expenses = read_lines(file_name)
+    return next(first * second * (2020 - first - second) for second in whole_list_of_expenses for first in whole_list_of_expenses if 2020 - first - second in whole_list_of_expenses)
 
 print("First part: ", find_values_that_sum_is_2020(l))
 print("Second part: ", find_three_values_that_sum_2020(l))
