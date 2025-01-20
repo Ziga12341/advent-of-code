285332179b2a1bd82ea92d7b449b1bf2c1152c64
Žiga Pregelj <ziga.pregelj@razum.si>
2025-01-20 20:11:53 +0100

    feat: 2020/day 3 done

---
+++ 2020/Day 03/main.py
@@ -0,0 +1,75 @@
+import unittest
+
+s = "small_input.txt"
+l = "input.txt"
+
+# for part two you should use this list of slopes
+# first is right second is down
+slopes = [
+    (1, 1),
+    (3, 1),  # this one is for part one
+    (5, 1),
+    (7, 1),
+    (1, 2)]
+
+
+def read_lines(file_name: str) -> list:
+    with open(file_name, "r", encoding="utf-8") as file:
+        return [line.strip() for line in file]
+
+
+def count_trees_three_right_one_down(file_name):
+    trees = 0
+    position_in_line = 0
+    grid = read_lines(file_name)
+    grid_wide = len(grid[0])
+    # do not start with the first line (start with the second)
+    for line in grid[1:]:
+        position_in_line += 3
+        # modulus operator, % (modulo) operator also called the remainder
+        # I use this because when end of line you need to add the whole grid on the right
+        trees += line[position_in_line % grid_wide] == "#"
+    return trees
+
+
+def count_trees_move_n_right_and_m_down(file_name, right=3, down=1):
+    trees = 0
+    position_in_line = 0
+    # take grid lines by step (if step 2 take each second line)
+    grid = read_lines(file_name)[::down]
+    # how "big" is grid in line (x-axis)
+    grid_wide = len(grid[0])
+    # do not start with the first line (start with the second)
+    # initialize down steps in lines
+    for line in grid[1:]:
+        position_in_line += right
+        # modulus operator, % (modulo) operator also called the remainder
+        # I use this because when end of line you need to add the whole grid on the right
+        trees += line[position_in_line % grid_wide] == "#"
+    return trees
+
+
+def multiply_counted_trees_different_slopes_for_the_same_grid(file_name):
+    multiply_trees_each_slope = 1
+    for right, down in slopes:
+        multiply_trees_each_slope *= count_trees_move_n_right_and_m_down(file_name, right, down)
+    return multiply_trees_each_slope
+
+
+print("First part: ", count_trees_move_n_right_and_m_down(l))
+print("Second part: ", multiply_counted_trees_different_slopes_for_the_same_grid(l))
+
+
+class TestFunctions(unittest.TestCase):
+    def setUp(self):
+        self.small_input: list[str] = read_lines(s)
+        self.s = "small_input.txt"
+        self.l = "input.txt"
+
+    def test_part_1(self):
+        self.assertEqual(count_trees_move_n_right_and_m_down(self.s), 7)
+        self.assertEqual(count_trees_move_n_right_and_m_down(self.l), 254)
+
+    def test_part_2(self):
+        self.assertEqual(multiply_counted_trees_different_slopes_for_the_same_grid(self.s), 336)
+        self.assertEqual(multiply_counted_trees_different_slopes_for_the_same_grid(self.l), 1666768320)
